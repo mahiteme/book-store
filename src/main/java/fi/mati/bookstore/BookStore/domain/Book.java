02f84c87d6cbbbf78e753d889518846e4aaf950d
Mahitemesilasie <mahiteme@Mahitemesilasies-MacBook-Air.local>
2017-09-15 14:44:44 +0300

    First commit

---
+++ src/main/java/fi/mati/bookstore/BookStore/domain/Book.java
@@ -0,0 +1,60 @@
+package fi.mati.bookstore.BookStore.domain;
+
+public class Book {
+	private String title;
+	private String autor;
+	private int year;
+	private String isbn;
+	private double price;
+	
+	public Book(String title, String autor, int year, String isbn, double price) {
+		super();
+		this.title = title;
+		this.autor = autor;
+		this.year = year;
+		this.isbn = isbn;
+		this.price = price;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+
+}
---
+++ src/main/java/fi/mati/bookstore/BookStore/web/BookController.java
@@ -0,0 +1,5 @@
+package fi.mati.bookstore.BookStore.web;
+
+public class BookController {
+
+}
